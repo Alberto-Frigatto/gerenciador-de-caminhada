95cb76942bfbdfd71e713377880212c160ab48f8
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2023-03-25 12:01:33 -0300

    Criadas as funções das opções do app mas vazias

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import app.dao
+from colorama import Fore
 
 
 class App:
@@ -8,3 +9,27 @@ class App:
     def _create_walking_list_file_if_not_exists(self) -> None:
         if not app.dao.walking_list_file_exist():
             app.dao.create_walking_list_file()
+
+    def view_walks(self):
+        pass
+
+    def create_walk(self):
+        pass
+
+    def mean_walking_time(self):
+        pass
+
+    def walking_time_std(self):
+        pass
+
+    def total_mileage(self):
+        pass
+
+    def mean_monthly_mileage(self):
+        pass
+
+    def mean_monthly_mileage_plot(self):
+        pass
+
+    def walking_time_plot(self):
+        pass
