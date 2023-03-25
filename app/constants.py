4e8f97a89725e0d1b1d17aefbdcc14bfb7842878
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2023-03-25 11:19:32 -0300

    Alterada a constante de colunas da lista de caminhada para um enum

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,12 @@
+from enum import Enum
+
+
 WALKING_LIST_DIRECTORY = 'data'
 WALKING_LIST_FILE_NAME = 'walking_list.csv'
-WALKING_LIST_COLUMNS = ('Data', 'Distância', 'Duração')
 CSV_SEPARATOR = ';'
+
+
+class WalkingListColumns(Enum):
+    DATE = 'Data'
+    DISTANCE = 'Distãncia'
+    DURATION = 'Duração'
