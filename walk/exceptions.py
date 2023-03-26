630c9110ee1d0762e49c5419b3ce9302acb2eb27
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2023-03-26 20:33:07 -0300

    Adicionado \n ao final de cada mensagem de exceção do pacote walk

--- walk/exceptions.py
+++ walk/exceptions.py
@@ -8,21 +8,21 @@ class WalkError(Exception, metaclass=ABCMeta):
 
 
 class WalkDateError(WalkError):
-    MESSAGE = f'{Fore.RED}Data inválida{Fore.RESET}'
+    MESSAGE = f'{Fore.RED}Data inválida{Fore.RESET}\n'
 
     def __init__(self) -> None:
         super().__init__(self.MESSAGE)
 
 
 class WalkDistanceError(WalkError):
-    MESSAGE = f'{Fore.RED}Distância inválida{Fore.RESET}'
+    MESSAGE = f'{Fore.RED}Distância inválida{Fore.RESET}\n'
 
     def __init__(self) -> None:
         super().__init__(self.MESSAGE)
 
 
 class WalkDurationError(WalkError):
-    MESSAGE = f'{Fore.RED}Duração inválida{Fore.RESET}'
+    MESSAGE = f'{Fore.RED}Duração inválida{Fore.RESET}\n'
 
     def __init__(self) -> None:
         super().__init__(self.MESSAGE)
