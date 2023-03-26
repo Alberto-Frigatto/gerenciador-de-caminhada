55ba0e2edc8b73cc4b09cc64ef2451da58e91770
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2023-03-26 13:25:49 -0300

    Criada constante para o caminho do arquivo da lista de caminhada

--- app/constants.py
+++ app/constants.py
@@ -1,8 +1,12 @@
+import os
 from enum import StrEnum
 
 
 WALKING_LIST_DIRECTORY = 'data'
 WALKING_LIST_FILE_NAME = 'walking_list.csv'
+WALKING_LIST_FILE_PATH = os.path.join(
+    WALKING_LIST_DIRECTORY, WALKING_LIST_FILE_NAME
+)
 CSV_SEPARATOR = ';'
 
 
