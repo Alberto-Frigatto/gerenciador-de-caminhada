979a531d8a983aecf4d7223cf839ec64e4c9c11a
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2023-03-26 12:09:33 -0300

    Criadas exceções para futura classe Walk
    Foram criadas as seguintes exceções:
    
    WalkError - exceção base abstrata
    
    WalkDateError - se a data da caminhada for inválida
    
    WalkDistanceError - se a distância da caminhada for inválida
    
    WalkDurationError - se a duração da caminhada for inválida

---
+++ walk/exceptions.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta
+from colorama import Fore
+
+
+class WalkError(Exception, metaclass=ABCMeta):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class WalkDateError(WalkError):
+    MESSAGE = f'{Fore.RED}Data inválida{Fore.RESET}'
+
+    def __init__(self) -> None:
+        super().__init__(self.MESSAGE)
+
+
+class WalkDistanceError(WalkError):
+    MESSAGE = f'{Fore.RED}Distância inválida{Fore.RESET}'
+
+    def __init__(self) -> None:
+        super().__init__(self.MESSAGE)
+
+
+class WalkDurationError(WalkError):
+    MESSAGE = f'{Fore.RED}Duração inválida{Fore.RESET}'
+
+    def __init__(self) -> None:
+        super().__init__(self.MESSAGE)
