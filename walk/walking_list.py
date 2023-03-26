d1642472a917a59d206432daa403298a6d4c017c
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2023-03-26 13:48:15 -0300

    Criada a classe WalkingList
    Essa classe é responsável por manipular a lista de caminhada e será responsável por fornecer as estatísticas necessárias para o app.
    
    Inicialmente ela possui dois métodos:
    
    add_walk - para adicionar uma caminhada
    save - para salvar a lista no arquivo

---
+++ walk/walking_list.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import pandas as pd
+from .walk import Walk
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.join(current_dir, '..')
+sys.path.append(root_dir)
+
+from app.constants import (
+    CSV_SEPARATOR,
+    WALKING_LIST_FILE_PATH,
+    WalkingListColumns
+)
+
+
+class WalkingList:
+    def __init__(self) -> None:
+        self._df_walking_list = pd.read_csv(
+            WALKING_LIST_FILE_PATH,
+            sep=CSV_SEPARATOR
+        )
+
+    def add_walk(self, walk: Walk) -> None:
+        self._df_walking_list.loc[len(self._df_walking_list)] = {
+            WalkingListColumns.DATE: walk.date,
+            WalkingListColumns.DISTANCE: walk.distance,
+            WalkingListColumns.DURATION: walk.duration
+        }
+
+    def save(self) -> None:
+        self._df_walking_list.to_csv(
+            WALKING_LIST_FILE_PATH,
+            index=False
+        )
